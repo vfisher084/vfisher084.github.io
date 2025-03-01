4c7df59e2bc01c9010e6db6ae1875f4f71b72b67
vfisher084 <vfisher@email.sc.edu>
2025-03-01 14:55:06 -0500

    Added Assignment 9 and updated main page

---
+++ csce242/assignments/assignment09/script.js
@@ -0,0 +1,65 @@
+class Pizza {
+    constructor(name, ingredients, sauce, cheese, price, image) {
+        this.name = name;
+        this.ingredients = ingredients;
+        this.sauce = sauce;
+        this.cheese = cheese;
+        this.price = price;
+        this.image = image;
+    }
+
+    getSection() {
+        const section = document.createElement("section");
+        section.classList.add("pizza-box");
+        section.innerHTML = `
+            <h3>${this.name}</h3>
+            <img src="images/${this.image}" alt="${this.name}" class="pizza-image">
+            `;
+        section.onclick = () => this.showModal();
+        return section;
+    }
+
+    showModal() {
+        const modal = document.getElementById("pizzaModal");
+        const modalContent = document.getElementById("modalContent");
+        modalContent.innerHTML = `
+            <div class="w3-animate-top modal-content">
+                <div id="text-div">
+                    <h4>${this.name}</h4>
+                    <p><strong>Ingredients:</strong> ${this.ingredients}</p>
+                    <p><strong>Sauce:</strong> ${this.sauce}</p>
+                    <p><strong>Cheese:</strong> ${this.cheese}</p>
+                    <p><strong>Price:</strong> $${this.price}</p>
+                </div>
+                <div id="image-div">
+                    <img src="images/${this.image}" alt="${this.name}" class="pizza-image">
+                </div>
+            </div>
+            `;
+            modal.style.display = "block";
+    }
+}
+
+const pizzas = [
+    new Pizza("Hawaiian", "Ham, Pineapple", "Tomato Sauce", "Mozzarella", 20, "hawaiian.jpg"),
+    new Pizza("Buffalo Chicken Ranch", "Chicken, Onions", "Wing Sauce", "Mozzarella", 17, "buffalo-chicken.jpg"),
+    new Pizza("Margherita", "Basil, Tomatoes", "Extra Virgin Olive Oil", "Fresh Mozzarella", 19, "margherita.jpg"),
+    new Pizza("Pepperoni", "Pepperoni slices", "Tomato sauce", "Mozzarella", 16, "pepperoni.jpg"),
+    new Pizza("Veggie", "Peppers, Olives, Onions", "Tomato Sauce", "Mozzarella", 15, "veggie.jpg")
+];
+
+const main = document.querySelector('main');
+const pizzaContainer = document.createElement('div');
+pizzaContainer.classList.add('pizza-container');
+main.appendChild(pizzaContainer);
+
+pizzas.forEach(pizza => {
+    pizzaContainer.appendChild(pizza.getSection());
+})
+
+window.onclick = (event) => {
+    const modal = document.getElementById("pizzaModal");
+    if(event.target === modal) {
+        modal.style.display = "none";
+    }
+}
---
+++ csce242/exercises/07javascript/04_js/js/tile-modal.js
@@ -0,0 +1,7 @@
+document.querySelectorAll(".gallery section").forEach((tile)=>{
+    tile.onclick = () => {
+        const label = tile.querySelector("h4").innerHTML;
+        const imgSRC = tile.querySelector("img" .src);
+        console.log(label);
+    }
+});
