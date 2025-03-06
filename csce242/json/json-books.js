19ee0c7b3a62bcd8ac1126b7e0cecd31db62d112
vfisher084 <vfisher@email.sc.edu>
2025-03-06 08:56:34 -0500

    Created json books file for BookNook

---
+++ csce242/json/json-books.js
@@ -0,0 +1,123 @@
+//Book Details JSON file
+[
+    //Fourth Wing
+    {
+        "title": "Fourth Wing",
+        "author": "Rebecca Yarros",
+        "description": "Desc",
+        "price": "29.99",
+        "format": ["Hardcover", "Paperback", "e-Book"],
+        "image": "images/fourth-wing.jpg",
+        "genre": ["Romance", "Fiction", "Fantasy"],
+        "reviews": [
+            "Great book!",
+            "I loved it!"
+        ],
+        "quantity": 30
+    },
+    //When the Moon Hatched
+    {
+        "title": "When the Moon Hatched",
+        "author": "Sarah A. Parker",
+        "description": "Desc",
+        "price": "19.99",
+        "format": ["Paperback", "e-Book"],
+        "image": "images/wtmh.jpg",
+        "genre": ["Romance", "Fiction", "Fantasy"],
+        "reviews": [
+            "Great book!",
+            "I loved it!"
+        ],
+        "quantity": 30
+    },
+    //The Hazel Wood
+    {
+        "title": "The Hazel Wood",
+        "author": "Melissa Albert",
+        "description": "Desc",
+        "price": "14.99",
+        "format": ["Hardcover", "Paperback", "e-Book"],
+        "image": "images/thehazelwood.jpg",
+        "genre": ["Fiction", "Fantasy", "Mystery"],
+        "reviews": [
+            "Great book!",
+            "I loved it!"
+        ],
+        "quantity": 30
+    },
+    //A Winter's Promise
+    {
+        "title": "A Winter's Promise",
+        "author": "Christelle Dabos",
+        "description": "Desc",
+        "price": "12.99",
+        "format": ["Paperback", "e-Book"],
+        "image": "images/winterspromise.jpg",
+        "genre": ["Fiction", "Fantasy", "Sci-Fi"],
+        "reviews": [
+            "Great book!",
+            "I loved it!"
+        ],
+        "quantity": 30
+    },
+    //The Housemaid's Secret
+    {
+        "title": "The Housemaid's Secret",
+        "author": "Freida McFadden",
+        "description": "Desc",
+        "price": "13.99",
+        "format": ["Paperback"],
+        "image": "images/housemaid.jpg",
+        "genre": ["Fiction", "Thriller", "Mystery"],
+        "reviews": [
+            "Great book!",
+            "I loved it!"
+        ],
+        "quantity": 30
+    },
+    //The Lion, the Witch and the Wardrobe
+    {
+        "title": "The Lion, the Witch and the Wardrobe",
+        "author": "C.S. Lewis",
+        "description": "Desc",
+        "price": "10.99",
+        "format": ["Paperback", "e-Book"],
+        "image": "images/narnia.jpg",
+        "genre": ["Fiction", "Fantasy"],
+        "reviews": [
+            "Great book!",
+            "I loved it!"
+        ],
+        "quantity": 30
+    },
+    //The Summer I Turned Pretty
+    {
+        "title": "The Summer I Turned Pretty",
+        "author": "Jenny Han",
+        "description": "Desc",
+        "price": "11.99",
+        "format": ["Paperback", "e-Book"],
+        "image": "images/tsitp.jpg",
+        "genre": ["Romance", "Fiction", "Young Adult"],
+        "reviews": [
+            "Great book!",
+            "I loved it!"
+        ],
+        "quantity": 30
+    },
+    //The Frozen River
+    {
+        "title": "The Frozen River",
+        "author": "Ariel Lawhon",
+        "description": "Desc",
+        "price": "18.99",
+        "format": ["Paperback", "e-Book"],
+        "image": "images/frozenriver.jpg",
+        "genre": ["Fiction", "Mystery"],
+        "reviews": [
+            "Great book!",
+            "I loved it!"
+        ],
+        "quantity": 30
+    },
+]
